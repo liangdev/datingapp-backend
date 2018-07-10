7922fe04fceac3f742cbcf81cff73094f1b554fe
liangdev <liangdev@yahoo.com>
2018-07-10 16:34:43 +0800

    test

---
+++ test/test.js
@@ -0,0 +1,13 @@
+var assert = require('assert');
+describe('Array', function() {
+  describe('#indexOf()', function() {
+
+    it('should return -1 when the value is not present', function() {
+      assert.equal([1,2,3].indexOf(4), -1);
+    });
+
+    it('should return index when the value is not present', function() {
+      assert.equal([1,2,3].indexOf(1), 0);
+    });
+  });
+});
